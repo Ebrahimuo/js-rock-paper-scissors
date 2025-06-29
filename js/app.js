75bfc0501452a67528d903b4f90a89be2b879f4c
Ebrahim User <ebrahimuo@gmail.com>
2025-06-29 10:27:53 +0300

    Finish scffolding html and css

---
+++ js/app.js
@@ -0,0 +1,26 @@
+/*-------------------------------- Constants --------------------------------*/
+
+const choices = ['rock', 'paper', 'scissors'];
+
+
+/*-------------------------------- Variables --------------------------------*/
+
+
+let playerChoice;
+let computerChoice; 
+let msg;
+
+/*------------------------ Cached Element References ------------------------*/
+
+const resultDisplayEl = document.querySelector('#result-display');
+
+
+/*-------------------------------- Functions --------------------------------*/
+
+
+
+/*----------------------------- Event Listeners -----------------------------*/
+
+document.querySelector('#rock').addEventListener('click', play);
+document.querySelector('#paper').addEventListener('click', play);
+document.querySelector('#scissors').addEventListener('click', play);
